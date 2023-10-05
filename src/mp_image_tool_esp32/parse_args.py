ce6e84a9b081d70393503a6fc81b9e688a0cbe62
Glenn Moloney <glenn.moloney@gmail.com>
2023-10-05 14:50:32 +1100

    Add support for parsing progname and description.

--- src/mp_image_tool_esp32/parse_args.py
+++ src/mp_image_tool_esp32/parse_args.py
@@ -1,6 +1,12 @@
 # MIT License: Copyright (c) 2023 @glenn20
 
 import argparse
+from itertools import takewhile
+
+MB = 0x100_000  # 1 Megabyte
+KB = 0x400  # 1 Kilobyte
+B = 0x1_000  # 1 Block (4096 bytes)
+SIZE_UNITS = {"M": MB, "K": KB, "B": B}
 
 actions = {
     "S": "store",
@@ -14,15 +20,30 @@ actions = {
 }
 
 
-def parse_args(cmd_args: str) -> argparse.Namespace:
-    parser = argparse.ArgumentParser()
+# arguments is a multiline string of the form:
+# progname
+# Description paragraph
+#
+# argument1               | help string     | action
+# argument2               | help string     | action
+# -a --option1            | help string     | action
+# -b --option2 NAME       | help string     | action
+# ...
+# Where action is a key from actions dict above.
+def parser(arguments: str) -> argparse.ArgumentParser:
+    # Split the arguments string up into lines for processing
+    lines = (s.strip() for s in arguments.strip().splitlines())
+    prog = next(lines)
+    description = " ".join(takewhile(lambda s: s, lines))
+    parser = argparse.ArgumentParser(prog=prog, description=description)
+
     # Lines of cmd_args are like: "arguments | help | action"
     # "filename | filename for output | T" # T means action="store_true"
     # "-i --input FILE | read input from FILE"
     # "-q --quiet | suppress normal output | T"  # action="store_true"
     # opts holds all the arguments starting with "-"
-    # args holds all the other arguments
-    for opts, args, help, action in [
+    # metavars holds all the other arguments
+    for opts, metavars, help, action in [
         [
             [s for s in args.split() if s[0] == "-"],  # args starting with "-"
             [s for s in args.split() if s[0] != "-"],  # other args
@@ -30,22 +51,22 @@ def parse_args(cmd_args: str) -> argparse.Namespace:
             action,
         ]
         for args, help, action, *_ in [  # Split each line at "|" boundaries
-            [s.strip() for s in (line + "|").split("|")]
-            for line in (s for s in cmd_args.strip().splitlines() if s.strip())
+            [s.strip() for s in (line + "||").split("|")] for line in lines
         ]
     ]:
-        metavars = args if opts else []
-        args = opts or args
+        if not opts:  # No options - use metavars as list of options
+            opts = metavars
+            metavars = []
         kwargs = {
             k: v
             for k, v in {
                 "nargs": len(metavars) if len(metavars) > 1 else 0,
-                "metavar": metavars,
+                "metavar": metavars[0] if len(metavars) == 1 else metavars,
                 "help": help,
                 "action": actions.get(action, action),
             }.items()
             if v  # Only use keywords if they have values
         }
-        parser.add_argument(*args, **kwargs)
+        parser.add_argument(*opts, **kwargs)
 
-    return parser.parse_args()
+    return parser
