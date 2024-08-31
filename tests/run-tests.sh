58844a26ac4f6461301ed5fa49f7d8d36fd415b1
Glenn Moloney <glenn.moloney@gmail.com>
2024-08-31 23:08:07 +1000

    run-tests.sh: Use 'uv' to run tests for multiple Python versions.

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -1,51 +1,44 @@
 #!/bin/bash
 
-# Python versions to use for tests
-pyenv_versions="3.12.2 3.11.9 3.10.14 3.9.19 3.8.19"
+# Uses 'uv' to run tests for multiple Python versions
+# and/or firmware files. The 'uv' command can be installed
+# using pip, eg:
+#
+#  pip install --user uv
 
-# Ensure pyenv is available and initialised
-eval "$(pyenv init -)"
+# Python versions to use for tests
+pyversions="3.12 3.11 3.10 3.9 3.8"
 
 Usage() {
     echo "Usage: $0 [--install-versions] [--init]"
-    echo "  --install-pyversions: Install the Python versions"
-    echo "  --init-pyversions: Setup the Python versions"
-    echo "  --pyversion <version|all>: Run tests for specific Python versions"
     echo "  --firmware <file|all>: Run tests for specific firmware files"
-    echo "  --device <device>: Run tests for device on serial port"
+    echo "  --port <device>: Run tests for device on serial port"
     exit 1
 }
+
+if ! type -P uv >/dev/null; then
+    echo "uv is not installed"
+    echo "Please install uv using 'pip install --user uv'"
+    exit 1
+fi
 
 testdir=$(dirname $0)
 
 args=""
+versions=""
+files=""
 while [ $# -gt 0 ]; do
     case $1 in
-        --install-pyversions)
-            # Install the Python versions
-            for version in $pyenv_versions; do
-                pyenv install -s $version
-            done
-            ;;
-        --init-pyversions)
-            # Setup the Python versions
-            for version in $pyenv_versions; do
-                pyenv shell $version
-                pyenv exec pip install --upgrade pip
-                pyenv exec pip install pytest
-                pyenv exec pip install -r requirements.txt
-            done
-            ;;
-        --pyversion)
+        --version|-v)
             # Run the tests for the specified Python versions
             if [ "$2" = "all" ]; then
-                versions="$pyenv_versions"
+                versions="$pyversions"
             else
                 versions="$2"
             fi
             shift
             ;;
-        --firmware)
+        --firmware|-f)
             # Run the tests on the specified firmware
             if [ "$2" = "all" ]; then
                 files="$(ls ${testdir}/data/ESP*.bin)"
@@ -54,13 +47,16 @@ while [ $# -gt 0 ]; do
             fi
             shift
             ;;
-        --device)
+        --port|-p)
             # Run the tests on the specified device
-            args="$args --device $2"
+            args="$args --port $2"
             shift
             ;;
         *)
             args="$args $1"
+            if [ "$1" = "-x" ]; then
+                set -o errexit -o pipefail -o noclobber -o nounset
+            fi
             ;;
     esac
     shift
@@ -71,8 +67,7 @@ if [ -n "$versions" ]; then
     echo "Running tests for Python versions: $versions"
     for version in $versions; do
         echo "Running tests for Python $version"
-        pyenv shell $version
-        pyenv exec python -m pytest $args
+        uv run --python $version pytest $args
     done
     exit 0
 fi
@@ -82,10 +77,10 @@ if [ -n "$files" ]; then
     for f in $files; do
         name=$(basename $f)
         echo "Running tests for firmware file: $name"
-        pytest $args --firmware $name
+        uv run pytest $args --firmware $name
     done
     exit 0
 fi
 
-pytest $args
+uv run pytest $args
 
